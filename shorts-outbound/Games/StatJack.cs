a62c608fddca3fff9311f1ac92c7a995ea2ec9b9
Christop Usbeck <chrtistopusbeck@gmail.com>
2026-05-03 23:37:15 +0200

    Added stubs for some game implementations

---
+++ shorts-outbound/Games/StatJack.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class StatJack : Node2D, IBaseGame
+{
+	private int _currentGeneration;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public void StartGame()
+	{
+		GD.Print("Game Logic Starting Now!");
+		// Initialize your cards, spawners, or timers here
+	}
+
+	// This is called by MainApp.cs when the slider moves 
+	// OR immediately before StartGame()
+	public void SetGeneration(int gen)
+	{
+		_currentGeneration = gen;
+		GD.Print($"Game updated to Generation: {gen}");
+		// Update your game visuals here
+	}
+
+	public void StopGame()
+	{
+		GD.Print("Cleaning up game state...");
+	}
+}
